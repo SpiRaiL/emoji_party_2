4be4397661d6b4c453728e37d586346aa4103e71
David Cox <david@humanssince1982.com>
2022-10-26 00:16:24 +0200

    Move all selected together. Widget resize

--- lib/model/block.dart
+++ lib/model/block.dart
@@ -7,6 +7,169 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_emoji/flutter_emoji.dart';
 
+class BlockSet {
+  /// A class for all the relevant block data as a full group
+  List<Block> allBlocks = [];
+  List<Block> selectedBlocks = [];
+
+  /// [setStateCallback] is currently needed for dragging a selected group
+  /// This is because setState at this level is only able to rerender the
+  /// widget being dragged.
+  Function setStateCallback;
+
+  BlockSet({required this.setStateCallback}) {
+    addBlock();
+  }
+
+  void addBlock() {
+    /// Add a block to the list of blocks
+    allBlocks.add(Block(generate: true, emoji: true, blockSet: this));
+  }
+
+  void deleteSelected() {
+    // delete the selected blocks
+    allBlocks.removeWhere((block) => selectedBlocks.contains(block));
+    selectedBlocks.clear();
+  }
+
+  void selectAllOrNone() {
+    /// If they are all selected
+    if (selectedBlocks.length == allBlocks.length) {
+      /// Select None
+      selectedBlocks.clear();
+    } else {
+      /// Select all
+      selectedBlocks.clear();
+      selectedBlocks.addAll(allBlocks);
+    }
+  }
+
+  List<Block> selectedInOrder() {
+    /// Get the selected order based on where the block is in allBlocks
+    /// ie: since selected blocks is ordered based when the user clicks on it
+    List<Block> selection = [];
+    for (Block block in allBlocks) {
+      if (selectedBlocks.contains(block)) {
+        selection.add(block);
+      }
+    }
+    return selection;
+  }
+
+  bool _areAllSelected({onTop = false, onBottom = false}) {
+    /// Returns true if the selected items are already on top of the stack
+    bool itemBeforeSelected = false;
+    bool itemAfterSelected = false;
+    bool selectedFound = false;
+
+    for (Block block in allBlocks) {
+      if (selectedBlocks.contains(block)) {
+        selectedFound = true;
+      } else {
+        if (!selectedFound) {
+          itemBeforeSelected = true;
+        } else {
+          itemAfterSelected = true;
+        }
+      }
+    }
+    if (onTop && itemAfterSelected) {
+      return false;
+    }
+    if (onBottom && itemBeforeSelected) {
+      return false;
+    }
+    return true;
+  }
+
+  void toTop({preserveOrder = true, exceptWhenOnTop = true}) {
+    /// Move selected items to the top of the stack
+    /// ie: the end of the list
+    /// Using [preserveOrder] keeps the order of the blocks intact
+    /// [exceptWhenOnTop] breaks the preservation if they are already on top
+    /// And falls back to the selected order "first clicked is on top"
+
+    if (exceptWhenOnTop && _areAllSelected(onTop: true)) {
+      preserveOrder = false;
+    }
+
+    List<Block> selection =
+        preserveOrder ? selectedInOrder() : selectedBlocks.reversed.toList();
+
+    /// Place blocks on the top based on their order in the selection
+    /// Last block is the top block
+    for (Block block in selection) {
+      int index = allBlocks.indexOf(block);
+      allBlocks.insert(allBlocks.length - 1, allBlocks.removeAt(index));
+    }
+  }
+
+  void toBottom({preserveOrder = true, exceptWhenOnBottom = true}) {
+    /// Move selected items to the bottom of the stack
+    /// ie: the start of the list
+    /// Using [preserveOrder] keeps the order of the blocks intact
+    /// [exceptWhenOnBottom] breaks the preservation if they are already on top
+    /// And falls back to the selected order
+    /// The default is false, which means the new order is based on
+    /// And falls back to the selected order "first clicked is on bottom"
+
+    if (exceptWhenOnBottom && _areAllSelected(onBottom: true)) {
+      preserveOrder = false;
+    }
+
+    List<Block> selection = preserveOrder ? selectedInOrder() : selectedBlocks;
+
+    for (Block block in selection.reversed) {
+      int index = allBlocks.indexOf(block);
+      allBlocks.insert(0, allBlocks.removeAt(index));
+    }
+  }
+
+  void reGenerate() {
+    /// Re-roll the emoji, in the same place in the stack
+    for (Block block in selectedBlocks) {
+      block.generateData(emoji: true);
+    }
+  }
+
+  ///
+  /// Selection functions
+  ///
+  bool selectBlock(Block block, bool trueFalse) {
+    /// Selects the [block] based on [trueFalse]
+    /// Returns true on success
+    /// False if already added or removed
+    if (trueFalse) {
+      if (!selectedBlocks.contains(block)) {
+        selectedBlocks.add(block);
+        return true;
+      }
+    } else {
+      if (selectedBlocks.contains(block)) {
+        selectedBlocks.remove(block);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  bool toggleSelectBlock(Block block) {
+    /// Toggles if the block is part of selected blocks
+    /// [returns] if the block is selected
+    if (selectedBlocks.contains(block)) {
+      selectedBlocks.remove(block);
+      return false;
+    } else {
+      selectedBlocks.add(block);
+      return true;
+    }
+  }
+
+  bool isBlockSelected(Block block) {
+    return selectedBlocks.contains(block);
+  }
+}
+
 /// A big list of emojis
 List<String> _emojiList = [];
 List<String> _emojiNames = [];
@@ -39,17 +202,40 @@ class Block {
   Color color;
   String displayName;
   String details;
-  bool selected = false;
+
+  /// Offset is the position of the block.
+  /// It needs to be store in the data as it needs to be accessible
+  /// by multiple widgets when grouping
+  Offset offset = Offset.zero;
+  Size size = const Size(100, 100);
+
+  /// A reference to the block set where this blocks belongs is required to
+  /// be able to make changes on other blocks
+  /// for example blocks in a selected group
+  final BlockSet blockSet;
 
   Block(
       {this.color = Colors.white,
       this.displayName = "None",
       this.details = "None",
       generate = false,
-      emoji = false}) {
+      emoji = false,
+      required this.blockSet}) {
     if (generate) {
       generateData(emoji: emoji);
     }
+  }
+
+  bool isSelected() {
+    return blockSet.isBlockSelected(this);
+  }
+
+  void select(bool trueFalse) {
+    blockSet.selectBlock(this, trueFalse);
+  }
+
+  void toggleSelect() {
+    blockSet.toggleSelectBlock(this);
   }
 
   generateData({emoji = false}) {
--- lib/widget/block_widget.dart
+++ lib/widget/block_widget.dart
@@ -1,6 +1,9 @@
 library block_library;
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../model/block.dart';
 
 class BlockWidget extends StatefulWidget {
@@ -9,45 +12,181 @@ class BlockWidget extends StatefulWidget {
   /// It allows ofr the movement of the Block.
   /// Below is the Block internal widget.
 
-  const BlockWidget({required this.blockData, super.key});
-  final Block blockData;
+  const BlockWidget({required this.block, super.key});
+  final Block block;
 
   @override
   State<BlockWidget> createState() => _BlockWidgetState();
 }
 
 class _BlockWidgetState extends State<BlockWidget> {
-  /// Where the x,y values for the block are stored
-  double _x = 0;
-  double _y = 0;
-
   /// Offset to account for the extra y position of the app-bar.
   /// this should be improved
-  double appBarHeight = 50;
+  final double appBarHeight = 50;
+
+  // The size of edge where the user can resize the widget
+  final double sizeHandleSize = 10;
+  final double minimumBlockSize = 25;
 
   @override
   Widget build(BuildContext context) {
+    Block block = widget.block;
     return Positioned(
-        left: _x,
-        top: _y,
+        left: block.offset.dx,
+        top: block.offset.dy,
         child: InkWell(
           onTap: () {
             setState(() {
-              widget.blockData.selected = !widget.blockData.selected;
+              block.toggleSelect();
             });
           },
-          child: Draggable(
-              feedback:
-                  BlockInternal(blockData: widget.blockData, dragging: true),
-              onDragEnd: (dragDetails) {
-                setState(() {
-                  _x = dragDetails.offset.dx;
-                  _y = dragDetails.offset.dy - appBarHeight;
-                });
-              },
-              child:
-                  BlockInternal(blockData: widget.blockData, dragging: false)),
+          child: Stack(children: [
+            Draggable(
+
+                /// Check if selected here
+                feedback: block.isSelected()
+                    ? BlockSelectedInternal(block: block)
+                    : BlockInternal(block: block, dragging: true),
+                onDragEnd: (dragDetails) {
+                  /// Get the distance moved
+                  Offset offsetDiff =
+                      dragDetails.offset.translate(0, -appBarHeight) -
+                          block.offset;
+
+                  if (block.isSelected()) {
+                    /// Move and update all the selected blocks
+                    for (Block block in block.blockSet.selectedBlocks) {
+                      block.offset += offsetDiff;
+                    }
+                    block.blockSet.setStateCallback();
+                  } else {
+                    /// Just move and update this block
+                    setState(() {
+                      block.offset += offsetDiff;
+                    });
+                  }
+                },
+                child: BlockInternal(block: block, dragging: false)),
+            _resizeHandle(SystemMouseCursors.resizeLeft),
+            _resizeHandle(SystemMouseCursors.resizeRight),
+            _resizeHandle(SystemMouseCursors.resizeUp),
+            _resizeHandle(SystemMouseCursors.resizeDown),
+          ]),
         ));
+  }
+
+  Positioned _resizeHandle(SystemMouseCursor direction) {
+    /// Generates an area on the side of the widget that
+    /// allows it to be resized along that axis
+    Block block = widget.block;
+
+    // Shorthand for directions
+    bool left = direction == SystemMouseCursors.resizeLeft;
+    bool right = direction == SystemMouseCursors.resizeRight;
+    bool up = direction == SystemMouseCursors.resizeUp;
+    bool down = direction == SystemMouseCursors.resizeDown;
+
+    return Positioned(
+
+        /// Where the handle is inside the block
+        left: left
+            ? 0
+            : right
+                ? block.size.width - sizeHandleSize
+                : sizeHandleSize, // up and down
+        top: up
+            ? 0
+            : down
+                ? block.size.height - sizeHandleSize
+                : sizeHandleSize,
+        child: MouseRegion(
+          cursor: direction,
+          child: GestureDetector(
+              onHorizontalDragUpdate: ((details) =>
+                  left || right ? _updateSize(direction, details) : null),
+              onVerticalDragUpdate: ((details) =>
+                  up || down ? _updateSize(direction, details) : null),
+              child: Container(
+                /// The size of the handle itself
+                width: left || right
+                    ? sizeHandleSize
+                    : block.size.width - 2 * sizeHandleSize,
+                height: up || down
+                    ? sizeHandleSize
+                    : block.size.height - 2 * sizeHandleSize,
+
+                /// Change the color here to see the relevant area
+                color: Colors.transparent,
+              )),
+        ));
+  }
+
+  void _updateSize(SystemMouseCursor direction, DragUpdateDetails details) {
+    /// Called from the resize handles to real-time update the size of the
+    /// widget.
+    Block block = widget.block;
+    // Shorthand for directions
+    bool left = direction == SystemMouseCursors.resizeLeft;
+    bool up = direction == SystemMouseCursors.resizeUp;
+    return setState(() {
+      if (left || up) {
+        block.offset = Offset(block.offset.dx + details.delta.dx,
+            block.offset.dy + details.delta.dy);
+      }
+
+      block.size = Size(
+          max(minimumBlockSize,
+              block.size.width + details.delta.dx * (left ? -1 : 1)),
+          max(minimumBlockSize,
+              block.size.height + details.delta.dy * (up ? -1 : 1)));
+    });
+  }
+}
+
+class BlockSelectedInternal extends StatelessWidget {
+  /// This is used when we drag a selected item
+  /// Rather than showing just one BlockInternal dragging,
+  /// We also need to show all the other selected blocks dragging
+
+  const BlockSelectedInternal({required this.block, super.key});
+  final Block block;
+
+  @override
+  Widget build(BuildContext context) {
+    /// A large container housing is needed to allow this stack to exist in the
+    /// parent draggable stack. Otherwise the system crashes.
+    /// It has something to do with other widgets not knowing what size to be.
+    ///
+    /// The next issue is that the container starts at the top left of the
+    /// dragging widget. As such no selected items above or left will be visible
+    /// To solve this we translate the entire container back to top-left corner
+    /// of the page. (try with Debug painting)
+    /// This is easy info, since we have stored this value in the blocks
+    /// co-ordinates. Theoretically a Positioned widget would solve this.
+    /// However positioned has the same issue as above. As such, we
+    /// can solve this instead using the transform function in the Container.
+    return Container(
+        height: 5000,
+        width: 5000,
+        transform:
+            Matrix4.translationValues(-block.offset.dx, -block.offset.dy, 0),
+        clipBehavior: Clip.none,
+        child: Stack(
+            children: block.blockSet
+                .selectedInOrder()
+                .map<Widget>((selectBlock) => Positioned(
+
+                    /// This "should" be the difference between this
+                    /// blocks position and the one that we started dragging
+                    /// however, the difference is already taken into account
+                    /// by the Matrix4.translation.
+                    left: selectBlock.offset.dx,
+                    top: selectBlock.offset.dy,
+                    child: BlockInternal(
+                      block: selectBlock,
+                      dragging: true,
+                    )))
+                .toList()));
   }
 }
 
@@ -58,29 +197,37 @@ class BlockInternal extends StatelessWidget {
   /// After trying to use the same instance for both jobs, it seems flutter
   /// just doesn't work like that
 
-  final Block blockData;
+  final Block block;
   final bool dragging;
+  // Internal block offset relative to parent
 
-  const BlockInternal(
-      {required this.blockData, this.dragging = false, super.key});
+  static const double resizeBorderWidth = 20;
+
+  const BlockInternal({
+    required this.block,
+    this.dragging = false,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Opacity(
-        opacity: dragging ? 0.5 : 1,
+        opacity: dragging ? 0.75 : 1,
         child: Material(
 
             /// Stops any text being red. Needs to be after opacity.
             child: Container(
+                height: block.size.height * (dragging ? 1.2 : 1),
+                width: block.size.width * (dragging ? 1.2 : 1),
 
                 /// Allows box decoration and padding
                 decoration: BoxDecoration(
-                  color: blockData.color,
+                  color: block.color,
 
                   /// The block boarder is only highlighted if the block is selected
                   border: Border.all(
                       width: 3,
-                      style: blockData.selected
+                      style: block.isSelected()
                           ? BorderStyle.solid
                           : BorderStyle.none,
                       color: const Color.fromARGB(255, 255, 107, 107)),
@@ -96,11 +243,13 @@ class BlockInternal extends StatelessWidget {
                   ],
                 ),
                 padding: const EdgeInsets.all(5),
-                child: Tooltip(
-                    message: blockData.details,
-                    child: Text(
-                      blockData.displayName,
-                      style: TextStyle(fontSize: dragging ? 30 : 25),
-                    )))));
+                child: FittedBox(
+                    fit: BoxFit.contain,
+                    child: Tooltip(
+                        message: block.details,
+                        child: Text(
+                          block.displayName,
+                          // style: TextStyle(fontSize: dragging ? 30 : 25),
+                        ))))));
   }
 }
